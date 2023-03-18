1f3fdcb0754d352bffc5faaa44f889b4aedac77f
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-03-18 17:22:20 +0100

    Tried something different

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,53 @@ import ArticleList from "./components/ArticleList";
 import { ArticlesContainer } from "./styles";
 import fetcher from "./lib/fetcher";
 import { SectionTitle } from "./styles";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { LoadingSpinner } from "./components/Looaders";
+import { ArticleCard } from "./components/ArticleCard";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${API_KEY}`;
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <ArticlesContainer>
+        <SectionTitle>Top Business Headline</SectionTitle>
+
+        <LoadingSpinner />
+      </ArticlesContainer>
+    );
+  }
+
   return (
     <>
       <ArticlesContainer>
         <SectionTitle>Top Business Headline</SectionTitle>
       </ArticlesContainer>
-      <SWRConfig value={{ fetcher, suspense: true }}>
-        <ArticlesContainer>
-          <ArticleList />
-        </ArticlesContainer>
-      </SWRConfig>
+      <ArticlesContainer>
+        {data.articles.map((data) => (
+          <ArticleCard
+            key={Math.floor(Math.random() * 999999)}
+            data={data}
+          />
+        ))}
+      </ArticlesContainer>
     </>
   );
 }
